6eef3930f2e71be6b659011a6b309280b3d88a9d
Olivier Peeters <olivierpeeters73@gmail.com>
2020-07-31 09:06:39 +0200

    Chart 1 ok

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,255 @@
 // CHART DE TABLE1
-/* 
-let canvas1 = document.createElement("canvas");          // CREATION DU CANVAS
-canvas1.setAttribute("id", "canvas1");                  
-canvas1.setAttribute("height", "800");
-canvas1.setAttribute("width", "400");
-document.getElementById("table1").before(canvas1);      // PLACEMENT DU CANVAS AVANT TABLE1
-*/
+// CREATION DU CANVAS ET PLACEMENT DU CANVAS AVANT TABLE1
+
 
 document.getElementById("table1").insertAdjacentHTML("beforebegin", '<canvas id="canvas1" height="400" width="400"></canvas>');
 let canvas1 = document.getElementById ("canvas1");
 
 
-let myChart1 = new Chart(canvas1, {
-    type: 'line',
-    data: {
-      labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-      datasets: [{ 
-          data: [86,114,106,106,107,111,133,221,783,2478],
-          label: "Africa",
-          borderColor: "#3e95cd",
-          fill: false
-        }, { 
-          data: [282,350,411,502,635,809,947,1402,3700,5267],
-          label: "Asia",
-          borderColor: "#8e5ea2",
-          fill: false
-        }, { 
-          data: [168,170,178,190,203,276,408,547,675,734],
-          label: "Europe",
-          borderColor: "#3cba9f",
-          fill: false
-        }, { 
-          data: [40,20,10,16,24,38,74,167,508,784],
-          label: "Latin America",
-          borderColor: "#e8c3b9",
-          fill: false
-        }, { 
-          data: [6,3,2,2,7,26,82,172,312,433],
-          label: "North America",
-          borderColor: "#c45850",
-          fill: false
+// EXTRACTION DONNEES DU TABLE1
+
+
+let table = document.getElementById("table1");
+
+function tableToJson(table) {
+    let data = [];
+    for (i = 1; i < table.rows.length; i++) {
+        let tableRow = table.rows[i];
+        let rowData = [];
+        for (j = 1; j < tableRow.cells.length; j++) {
+            rowData.push(tableRow.cells[j].innerHTML);;
         }
+        data.push(rowData);
+    }
+    return data;
+}
+table = tableToJson(table);
+
+console.log(table);
+
+
+
+// POUR SUPPRIMER L'INDEX 0
+
+const arrayWithoutElementAtIndex = function(arr, index) {
+  let ret = arr.slice(); //make a copy
+  ret.splice(index, 1); //remove the item from given index
+  return ret; //return the copy
+}
+
+
+
+// POUR TRANSFORMER L'ARRAY DE STRING À FLOAT
+
+function arrayStringToFloat(table) {
+  data = [];
+  for (i = 0; i < table.length; i++) {
+    table[i] = table[i].replace(',','.');
+    data.push(table[i]);
+  }
+  return data;
+}
+
+// FONCTION POUR COULEURS RANDOM
+
+function randomColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  let rgb = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  return rgb
+}
+
+console.log(randomColor());
+
+
+let myChart1 = new Chart(canvas1, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+      labels: arrayWithoutElementAtIndex(table[0], 0),
+      datasets: [{ 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[1], 0)),
+          label: table[1][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[2], 0)),
+          label: table[2][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[3], 0)),
+          label: table[3][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[4], 0)),
+          label: table[4][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[5], 0)),
+          label: table[5][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[6], 0)),
+          label: table[6][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[7], 0)),
+          label: table[7][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[8], 0)),
+          label: table[8][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[9], 0)),
+          label: table[9][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[10], 0)),
+          label: table[10][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[11], 0)),
+          label: table[11][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[12], 0)),
+          label: table[12][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[13], 0)),
+          label: table[13][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[14], 0)),
+          label: table[14][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[15], 0)),
+          label: table[15][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[16], 0)),
+          label: table[16][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[17], 0)),
+          label: table[17][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[18], 0)),
+          label: table[18][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[19], 0)),
+          label: table[19][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[20], 0)),
+          label: table[20][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[21], 0)),
+          label: table[21][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[22], 0)),
+          label: table[22][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[23], 0)),
+          label: table[23][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[24], 0)),
+          label: table[24][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[25], 0)),
+          label: table[25][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[26], 0)),
+          label: table[26][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[27], 0)),
+          label: table[27][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[28], 0)),
+          label: table[28][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[29], 0)),
+          label: table[29][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[30], 0)),
+          label: table[30][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[31], 0)),
+          label: table[31][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[32], 0)),
+          label: table[32][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[33], 0)),
+          label: table[33][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[34], 0)),
+          label: table[34][0],
+          borderColor: randomColor(),
+          fill: false
+        }, { 
+          data: arrayStringToFloat(arrayWithoutElementAtIndex(table[35], 0)),
+          label: table[35][0],
+          borderColor: randomColor(),
+          fill: false
+        },
       ]
     },
+
+    // Configuration options go here
     options: {
       title: {
         display: true,
@@ -51,25 +258,32 @@ let myChart1 = new Chart(canvas1, {
     }
 });
 
-/*
-new Chart(canvas1, {
-    type: 'bar',
-    data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
+
+
+
+// CHART DE TABLE2
+// CREATION DU CANVAS ET PLACEMENT DU CANVAS AVANT TABLE2
+
+document.getElementById("table2").insertAdjacentHTML("beforebegin", '<canvas id="canvas2" height="400" width="400"></canvas>');
+let canvas2 = document.getElementById ("canvas2");
+
+// EXTRACTION DONNEES DU TABLE2
+
+
+let tab2 = document.getElementById("table2");
+
+function tableToJson(table) {
+    let data = [];
+    for (i = 1; i < table.rows.length; i++) {
+        let tableRow = table.rows[i];
+        let rowData = [];
+        for (j = 1; j < tableRow.cells.length; j++) {
+            rowData.push(tableRow.cells[j].innerHTML);;
         }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      }
+        data.push(rowData);
     }
-});
-*/
+    return data;
+}
+tab2 = tableToJson(tab2);
+
+console.log(tab2);
